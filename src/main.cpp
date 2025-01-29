79b8354bc61e9ed70861f0ba3bec33bbc00d6956
Caspar Klein <caspar.m.klein@gmail.com>
2025-01-29 22:14:23 +0100

    Fix shader loading issue by null terminating the loaded buffer

--- src/main.cpp
+++ src/main.cpp
@@ -21,8 +21,11 @@ char* filetobuf(const char *file)
     t.seekg(0, std::ios::end);    // go to the end
     length = t.tellg();           // report location (this is the length)
     t.seekg(0, std::ios::beg);    // go back to the beginning
-    buffer = (char*)malloc(length);    // allocate memory for a buffer of appropriate dimension
+    buffer = (char*)malloc(length+1);    // allocate memory for a buffer of appropriate dimension
     t.read(buffer, length);       // read the whole file into the buffer
+    buffer[length]='\0';
+    printf("%s",buffer
+    );
     t.close();
     return buffer;    
 }
