ec31d1befbe648876362a90d516762ffc70afa56
SpoxXxe <spoxxxe@gmail.com>
2025-02-11 20:38:04 +0100

    added source for model loading code

--- src/helpers/loader.cpp
+++ src/helpers/loader.cpp
@@ -4,7 +4,7 @@
 #include <string>
 
 namespace helpers{
-
+    //src: http://www.opengl-tutorial.org/beginners-tutorials/tutorial-7-model-loading/
     bool loadOBJ(const char * path, std::vector < glm::vec3 > & out_vertices, std::vector < glm::vec2 > & out_uvs, std::vector < glm::vec3 > & out_normals){
         std::vector< unsigned int > vertexIndices, uvIndices, normalIndices;
         std::vector< glm::vec3 > temp_vertices;
